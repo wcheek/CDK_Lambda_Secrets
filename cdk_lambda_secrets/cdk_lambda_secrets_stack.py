0a2789908e46e93883c129787579a971006dc9c0
Wesley Cheek <66931618+wcheek@users.noreply.github.com>
2022-05-13 15:01:45 +0900

    lambda with secrets

--- cdk_lambda_secrets/cdk_lambda_secrets_stack.py
+++ cdk_lambda_secrets/cdk_lambda_secrets_stack.py
@@ -1,19 +1,20 @@
-from aws_cdk import (
-    # Duration,
-    Stack,
-    # aws_sqs as sqs,
-)
+from aws_cdk import Duration, Stack
+from aws_cdk import aws_lambda as _lambda
 from constructs import Construct
 
-class CdkLambdaSecretsStack(Stack):
 
+class CdkLambdaSecretsStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        self.build_lambda_func()
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CdkLambdaSecretsQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+    def build_lambda_func(self):
+        self.processing_lambda = _lambda.Function(
+            scope=self,
+            id="LambdaWithSecrets",
+            runtime=_lambda.Runtime.PYTHON_3_9,
+            function_name="LambdaWithSecretsExample",
+            code=_lambda.Code.from_asset(path="lambda_with_secrets"),
+            handler="lambda_with_secrets.handler",
+        )
